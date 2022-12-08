1c08b6686e41510b010b596195658ed0e55ff5f7
Malva86 <105357204+Malva86@users.noreply.github.com>
2022-12-08 15:50:43 +0100

    apply moving icon

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Time from "./Time";
+import Icon from "./Icon";
 
 export default function Weather(props) {
   const [redy, setRedy] = useState(false);
@@ -13,7 +14,7 @@ export default function Weather(props) {
       city: response.data.name,
       humi: response.data.main.humidity,
       desc: response.data.weather[0].main,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
       date: new Date(response.data.dt * 1000),
     });
 
@@ -73,7 +74,7 @@ export default function Weather(props) {
 
         <div className="row Condition">
           <div className="col-md-6 Icon">
-            <img src={forecast.icon} alt="Icon" />
+            <Icon />
           </div>
           <div className="col-md-6">
             <ul>
